991f20825295966ee9d99c949af7d09b7316650d
TristanAntonsen <tristan.antonsen@gmail.com>
2022-08-20 21:58:54 -0500

    initializing and adding point & vector types

---
+++ src/constants.rs
@@ -0,0 +1,3 @@
+
+pub type point = [f32; 3];
+pub type vector = [f32; 4];
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+mod constants;
+use constants::{point, vector};
+fn main() {
+    println!("Hello, world!");
+
+    let test_point: point = [0.0, 0.0, 3.0];
+    let test_vector: vector = [0.0, 0.0, 3.0, 0.0];
+}

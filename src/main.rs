0f83a25a015742bee7ce119cb05217b9ff584d60
TristanAntonsen <tristan.antonsen@gmail.com>
2022-08-21 11:13:09 -0500

    kinda hacky clock array test

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ mod constants;
 mod export;
 mod tests;
 use constants::{point, vector, Canvas, Env, Projectile, Point, Vector};
-use nalgebra::Matrix1x4;
-use tests::{is_point, is_vector};
+use nalgebra::{Matrix1x4, Matrix4, Matrix4x1};
+use tests::{is_point, is_vector, clock};
 extern crate image;
 
 fn main() {
@@ -13,16 +13,12 @@ fn main() {
     let test_point = Matrix1x4::new(0.0, 0.0, 3.0, 1.0);
     let test_vector_2 = Vector::new(1.0, 1.0, 1.0);
 
-    let mut projectile_canvas = Canvas::new(500, 500);
-
-    let env = Env::new(0.050, 0.0);
-    let starting_position = Matrix1x4::new(0.0, 0.0, 0.0, 1.0);
-    let starting_velocity = Matrix1x4::new(3.5, 3.5, 0.0, 1.0);
-    let trajectory = tests::launch(projectile_canvas, env, starting_position, starting_velocity);
+    let mut clock_canvas = Canvas::new(500, 500);
 
 
     // projectile_canvas.write_pixel(2, 8, [1.0, 1.0, 1.0]);
+    let clock = clock(clock_canvas);
 
-
-    export::save_png("projectile.png", trajectory);
+    //translating starting poi
+    export::save_png("clock.png", clock);
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,7 +1,9 @@
-use nalgebra::Matrix1x4;
+use nalgebra::{Matrix1x4,Matrix4x1, Matrix4};
 use crate::Canvas;
 use crate::Env;
 use crate::Projectile;
+use crate::constants::point;
+use std::f64::consts::PI;
 
 pub fn is_point(tuple: Matrix1x4<f32>) -> bool {
     match tuple[3] {
@@ -16,6 +18,58 @@ pub fn is_vector(tuple: Matrix1x4<f32>) -> bool {
         _ => return false
     }
 }
+
+pub fn clock(mut canvas: Canvas) -> Canvas{
+
+    let divisions = 12;
+    let tau = 2.0 * PI;
+    let theta_increment = tau / divisions as f64;
+
+    let radius: f32 = canvas.pixels.len() as f32 * 0.375;
+    let mut point = Matrix4x1::new(radius,0.0,0.0,1.0);
+    let mut temp_point = Matrix4x1::new(0.0,0.0,0.0,1.0);
+    
+    let translation_matrix = Matrix4::new(
+        1.0, 0.0, 0.0, canvas.pixels.len() as f32 / 2.0,
+        0.0, 1.0, 0.0, canvas.pixels.len() as f32 / 2.0,
+        0.0, 0.0, 1.0, 0.0,
+        0.0, 0.0, 0.0, 1.0
+    );
+
+    let mut theta = theta_increment as f32;
+    let mut x;
+    let mut y;
+
+    let mut rotation_matrix = Matrix4::new(
+        1.0, 0.0, 0.0, 0.0,
+        0.0, 1.0, 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0,
+        0.0, 0.0, 0.0, 1.0
+    );
+
+    for _ in 0..divisions{
+        //creating new rotation matrix
+        rotation_matrix[0] = theta.cos(); //M[0,0]
+        rotation_matrix[1] = -theta.sin(); //M[0,1]
+        rotation_matrix[4] = theta.sin(); //M[1,0]
+        rotation_matrix[5] = theta.cos(); //M[1,1]
+
+        point = rotation_matrix * point;
+        temp_point = translation_matrix * point;
+        x = temp_point[0].round();
+        y = temp_point[1].round();
+
+        println!("x: {}, y: {}, theta: {}",x,y,(theta * 100.0).round() / 100.0);
+
+        canvas.write_pixel(x as usize, y as usize, [1.0,1.0,1.0]);
+
+        // theta += theta_increment;
+    };
+
+    return canvas
+
+}
+
 
 pub fn launch(mut canvas: Canvas, env: Env, position: Matrix1x4<f32>, velocity: Matrix1x4<f32>) -> Canvas {
 
@@ -42,7 +96,6 @@ pub fn launch(mut canvas: Canvas, env: Env, position: Matrix1x4<f32>, velocity: 
     return canvas
 
 }
-
 
 
 fn tick(env: &Env, mut proj: Projectile) -> Projectile {

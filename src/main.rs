e24e0b453715262026713e8309386de38c66c61e
TristanAntonsen <tristan.antonsen@gmail.com>
2022-08-20 22:16:24 -0500

    renaming to tests

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 mod constants;
-mod common;
+mod tests;
 use constants::{point, vector};
 use nalgebra::{Matrix1x4};
-use common::{is_point,is_vector};
+use tests::{is_point,is_vector};
 
 
 fn main() {
@@ -10,10 +10,10 @@ fn main() {
 
     let test_point = Matrix1x4::new(0.0, 0.0, 3.0,1.0);
     let test_vector = Matrix1x4::new(1.0,0.0,0.0,0.0);
+    let test_vector_2 = Matrix1x4::new(1.0,1.0,2.0,0.0);
 
-    println!("(true){}",is_point(test_point));
-    println!("(false){}",is_point(test_vector));
-    println!("(false){}",is_vector(test_point));
-    println!("(true){}",is_vector(test_vector));
+    let result = test_vector + test_vector_2;
+
+    println!("{}",result);
 }
  

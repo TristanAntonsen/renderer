438cd912f9aa3655d18e326e85d616334d626a33
TristanAntonsen <tristan.antonsen@gmail.com>
2022-08-20 22:23:01 -0500

    adding Point and Vector structs just in case

--- src/constants.rs
+++ src/constants.rs
@@ -3,3 +3,31 @@ use nalgebra::{Matrix1x3,Matrix1x4};
 pub type point = Matrix1x4<f32>;
 // pub type vector = [f32; 4];
 pub type vector = Matrix1x4<f32>;
+
+pub struct Point {
+    pub pos: [f32; 4]
+}
+
+impl Point {
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
+        Self {
+            pos: [
+                x, y, z, 1.0
+            ]
+        }
+    }
+}
+
+pub struct Vector {
+    pub dir: [f32; 4]
+}
+
+impl Vector {
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
+        Self {
+            dir: [
+                x, y, z, 0.0
+            ]
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod constants;
 mod tests;
-use constants::{point, vector};
+use constants::{point, vector, Point, Vector};
 use nalgebra::{Matrix1x4};
 use tests::{is_point,is_vector};
 
@@ -10,10 +10,11 @@ fn main() {
 
     let test_point = Matrix1x4::new(0.0, 0.0, 3.0,1.0);
     let test_vector = Matrix1x4::new(1.0,0.0,0.0,0.0);
-    let test_vector_2 = Matrix1x4::new(1.0,1.0,2.0,0.0);
+    let test_point_2 = Point::new(0.0, 0.0, 1.0);
+    let test_vector_2 = Vector::new(1.0,1.0,1.0);
 
-    let result = test_vector + test_vector_2;
+    println!("Point: {:?}",test_point_2.pos);
 
-    println!("{}",result);
+    println!("Vector: {:?}",test_vector_2.dir);
 }
  

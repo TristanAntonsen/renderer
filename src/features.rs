919e985a703bd41e228884089df3de5dd3fa33b5
TristanAntonsen <tristan.antonsen@gmail.com>
2022-08-27 15:45:51 -0500

    adding better comments

--- src/features.rs
+++ src/features.rs
@@ -1,38 +1,43 @@
 use crate::constants::{Ray, Sphere};
 use nalgebra::{Matrix1x4, Matrix4, Matrix4x1};
 
+// Calculating the position of an intersection (from distance t along a ray)
 pub fn position(mut ray: &Ray, t: &f32) -> Matrix1x4<f32> {
     let mut result = ray.origin + ray.direction * *t; //dereference t?
     result[3] = 0.0; //point type
 
     return result;
 }
-
+// Struct containing the t value (distance along ray) and the object the ray is intersecting
 #[derive(Debug)] //automatically implementing traits
 pub struct Intersection<'a> {
     pub t: f32,
     pub object: &'a Sphere,
 }
+
+// Struct for collecting many intersections including information about the objects
 pub struct Intersections<'a> {
     pub all: Vec<Intersection<'a>>,
 }
 
 impl<'a> Intersections<'a> {
+    // adding a new Intersection to Intersections
     pub fn add(&mut self, i: Intersection<'a>) {
         self.all.push(i);
     }
 }
 
-
 impl<'a> Intersection<'a> {
+    // creating a new intersection from t and the object (sphere for now)
     pub fn from_components(t: f32, object: &'a Sphere) -> Self {
         Self {
             t: t,
-            object: object
+            object: object,
         }
     }
 }
 
+// determine the intersection t values (t1, t2) or None from a ray and a sphere
 pub fn sphere_intersection(ray: &Ray, sphere: &Sphere) -> Option<(f32, f32)> {
     // vector from sphere origin to ray origin
     let sphere_to_ray = ray.origin - sphere.origin;

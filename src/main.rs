5a67917542777a5247eaecb911433f162ca2af00
TristanAntonsen <tristan.antonsen@gmail.com>
2022-09-11 20:47:34 -0500

    better comments explaining render loop

--- src/main.rs
+++ src/main.rs
@@ -23,35 +23,37 @@ extern crate image;
 
 fn main() {
 
-    // --------- Image parameters ----------
+    // -----------------------------------
+    // --------- Preparing scene ---------
+    // -----------------------------------
+
+    // --------- Image ----------
     const X_RES : u32 = 1080;
     const Y_RES : u32 = 1080;
+    let mut canvas = Canvas::new(X_RES as usize, Y_RES as usize);
+    let camera_origin = Matrix4x1::new(0.0, 0.0, 5.0, 1.0);
 
-
+    // --------- Objects ----------
+    // wall
     let wall_x : f32 = 6.0;
     let wall_y : f32 = 6.0;
     let wall_z : f32 = 10.0;
 
-    let mut canvas = Canvas::new(X_RES as usize, Y_RES as usize);
-
-
-    let camera_origin = Matrix4x1::new(0.0, 0.0, 5.0, 1.0);
-    
-    let mut ray = camera_ray(10.0, 10.0, camera_origin, wall_z, wall_x, wall_y);
-
     let mut sphere = Sphere::new(0.0, 0.0, 0.0, 1.0);
-    // sphere.set_transform(scaling(1.0, 1.0, 1.0));
-    sphere.set_transform(translation(0.0, 1.0, 0.0));
+    sphere.set_transform(scaling(2.0, 2.0, 2.0));
     
-
-    let mut intersections = Intersections{collection: Vec::new()};
-
+    
+    // --------- Initializing other variables ----------
     let x_inc = wall_x / X_RES as f32;
     let y_inc = wall_y / Y_RES as f32;
-
+    let mut ray: Ray;
     let mut canvas_x = 0.0;
     let mut canvas_y = 0.0;
-
+    
+    
+    // ------------------------------------
+    // --------- Main loop start ----------
+    // ------------------------------------
     for x in 0..X_RES {
         for y in 0..Y_RES {
             canvas_x = x as f32 * x_inc;
@@ -63,17 +65,11 @@ fn main() {
            }
         }
     }
+    // -----------------------------------------
+    // --------- Main render loop end ----------
+    // -----------------------------------------
 
-    let rt2_2 = (2.0 as f32).sqrt() / 2.0;
-    let eyev = Matrix4x1::new(0.0,0.0,-1.0,0.0);
-    let normalv = Matrix4x1::new(0.0,0.0,-1.0,0.0);
-    let light = PointLight::new(1.0, Matrix4x1::new(0.0,0.0,10.0,1.0));
-    let mut mat = Material::default();
-    let position = Matrix4x1::new(0.0,0.0,0.0,1.0);
-    let result = lighting(&mut mat, light, position, eyev, normalv);
-
-    println!("result: {}",result);
-
+    // --------- Saving render ---------- d
     _save_png("sphere.png", canvas);
 
 

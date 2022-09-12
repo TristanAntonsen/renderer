46b947657acb22e1ac4510f7661ed89e3ffdc7e4
TristanAntonsen <tristan.antonsen@gmail.com>
2022-09-11 20:05:00 -0500

    changing color to single value for now

--- src/material.rs
+++ src/material.rs
@@ -1,7 +1,7 @@
 use nalgebra::Matrix4x1;
 
 pub struct Material {
-    pub color: [f32; 3],
+    pub color: f32,
     pub ambient: f32,
     pub diffuse: f32,
     pub specular: f32,
@@ -9,7 +9,7 @@ pub struct Material {
 }
 
 impl Material {
-    pub fn new(color: [f32; 3], ambient: f32, diffuse: f32, specular: f32, shininess: f32) -> Self {
+    pub fn new(color: f32, ambient: f32, diffuse: f32, specular: f32, shininess: f32) -> Self {
         Self {
             color,
             ambient,
@@ -21,7 +21,7 @@ impl Material {
 
     pub fn default() -> Self {
         Self {
-            color: [1.0,1.0,1.0],
+            color: 1.0,
             ambient: 0.1,
             diffuse: 0.9,
             specular: 0.9,

c809798f184603410572eefc16dd8039fe0ee5cb
TristanAntonsen <tristan.antonsen@gmail.com>
2022-09-14 21:26:25 -0500

    adding new tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,34 @@
 mod constants;
 mod geometry;
 mod intersections;
-mod ray;
 mod light;
 mod material;
+mod ray;
+mod world;
 
 #[cfg(test)]
 mod tests {
-    use crate::geometry::{_Point, Sphere};
-    use crate::ray::Ray;
+    use crate::geometry::{Sphere, _Point};
+    use crate::intersections::{intersect_sphere, Intersection};
+    use crate::ray::{Ray,position};
     // use crate::intersections::{Intersection};
-    use nalgebra::Matrix1x4;
+    use nalgebra::{Matrix4, Matrix4x1};
 
     #[test]
-    fn test_point() {
-        let point = _Point{pos: [0.0, 0.0, 0.0, 1.0]};
-        assert_eq!(point.pos[3], 1.0);
+    fn test_sphere_intersection() {
+        //not sure if this is helpful
+        let ray = Ray {
+            origin: Matrix4x1::new(0.0, 0.0, -5.0, 1.0),
+            direction: Matrix4x1::new(0.0, 0.0, 1.0, 0.0),
+        };
+        let sphere = Sphere::new(0.0, 0.0, 0.0, 1.0);
+        println!("Hey");
+        if let Some(i) = intersect_sphere(&ray, &sphere) {
+            let p1 = position(&ray, i.0);
+            let p2 = position(&ray, i.1);
+            println!("Point 1: {:?}", p1);
+            println!("Point 2: {:?}", p2);
+        }
+        // assert_eq!(i1.object.origin, sphere.origin);
     }
-
-    // #[test]
-    // fn test_intersection() { //not sure if this is helpful
-    //     let ray = Ray {
-    //         origin: Matrix1x4::new(0.0, 0.0, 0.0, 1.0),
-    //         direction: Matrix1x4::new(1.0, 0.0, 0.0, 0.0)
-    //     };
-    //     let sphere = Sphere {
-    //         origin: Matrix1x4::new(5.0, 0.0, 0.0, 1.0),
-    //         radius: 1.0
-    //     };
-    //     let i1 = Intersection::new(&ray, &sphere);
-
-    //     assert_eq!(i1.object.origin, sphere.origin);
-    // }
-
 }

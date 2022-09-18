a09eb58aefbd69f537e46b3161fb7dd672e8ed8a
TristanAntonsen <tristan.antonsen@gmail.com>
2022-09-17 19:53:12 -0500

    test with 3 spheres

--- src/main.rs
+++ src/main.rs
@@ -28,49 +28,34 @@ fn main() {
     // --------- Preparing scene ---------
     // -----------------------------------
 
-    // --------- Image ----------
-    const X_RES: u32 = 1080;
-    const Y_RES: u32 = 1080;
-    let mut canvas = Canvas::new(X_RES as usize, Y_RES as usize);
-    let camera_origin = Matrix4x1::new(0.0, 0.0, -5.0, 1.0);
-
-    // --------- Wall ----------
-    let wall_x: f32 = 6.0;
-    let wall_y: f32 = 6.0;
-    let wall_z: f32 = 10.0;
-
+    
+    // --------- World -----------
+    let mut world = World::new();
 
     // --------- Light ----------
-    let mut light = PointLight::new(1.0, Matrix4x1::new(-10.0, -10.0, -10.0, 1.0));
+    let mut light = PointLight::new(1.0, Matrix4x1::new(10.0, 10.0, 5.0, 1.0));
     light.intensity = 1.0;
+    world.lights.push(light);
 
-    // --------- World -----------
-    let mut world = World::default();
-    let sphere_index = 0;
-    world.objects[sphere_index].material.color = color_from_rgb(255, 0, 0);
-    world.objects[sphere_index].set_transform(translation(0.0, 0.0, -2.0));
+    // --------- Objects -----------
+    let mut sphere_1 = Sphere::new(0.0, 0.0, 0.0, 1.0);
+    sphere_1.material.color = color_from_rgb(255, 0, 0);
+    sphere_1.transform = translation(0.0, 0.0, 0.0);
+    world.objects.push(sphere_1);
 
-    // --------- Initializing other variables ----------
-    let x_inc = wall_x / X_RES as f32;
-    let y_inc = wall_y / Y_RES as f32;
-    let mut ray: Ray;
-    let mut canvas_x = 0.0;
-    let mut canvas_y = 0.0;
-    let mut intersections: Intersections;
-    // let (mut color, mut point, mut normal, mut eye);
-    // let mut t;
+    let mut sphere2 = Sphere::new(0.0, 0.0, 0.0, 1.0);
+    sphere2.material.color = color_from_rgb(0, 255, 0);
+    sphere2.transform = translation(2.5,0.0, 0.0);
+    world.objects.push(sphere2);
+
+    let mut sphere3 = Sphere::new(0.0, 0.0, 0.0, 1.0);
+    sphere3.material.color = color_from_rgb(0, 0, 255);
+    sphere3.transform = translation(0.0,2.5, 0.0);
+    world.objects.push(sphere3);
 
     // --------- Camera ----------
-    let mut cam = Camera::default(500, 500, PI / 2.0);
-    // cam.transform = rotation_y(PI / 4.0) * translation(0.0, -2.0, 5.0);
-    println!("Camera params:");
-    println!("\thsize: {}", &cam.hsize);
-    println!("\tvsize: {}", &cam.vsize);
-    println!("\tpixel_size: {}", &cam.pixel_size);
-    println!("\thalf_width: {}", &cam.half_width);
-    println!("\thalf_height: {}", &cam.half_height);
-    println!("\tfov: {}", &cam.field_of_view);
-    println!("\ttransform: {:?}", &cam.transform);
+    let mut cam = Camera::default(500, 500, PI / 6.0);
+    cam.transform = translation(0.0, 0.0, -15.0);
 
     // --------- Testing ray_for_pixel ----------
 
@@ -81,43 +66,15 @@ fn main() {
     println!("ray direction: {:?}", cam_ray.direction);
 
     // --------- Test ray ----------
-    let test_ray = Ray {
-        origin: Matrix4x1::new(0.0, 0.0, 0.75, 1.0),
-        direction: Matrix4x1::new(0.0, 0.0, -1.0, 0.0),
-    };
+    // let test_ray = Ray {
+    //     origin: Matrix4x1::new(0.0, 0.0, 0.75, 1.0),
+    //     direction: Matrix4x1::new(0.0, 0.0, -1.0, 0.0),
+    // };
 
     // --------- Testing render() ----------
 
     let image = render(&cam, &world);
 
-    // ------------------------------------
-    // --------- Main loop start ----------
-    // ------------------------------------
-    // for x in 0..X_RES {
-    //     for y in 0..Y_RES {
-    //         canvas_x = x as f32 * x_inc;
-    //         canvas_y = y as f32 * y_inc;
-    //         ray = camera_ray(canvas_x, canvas_y, camera_origin, wall_z, wall_x, wall_y);
-            
-    //         if let Some(i) = intersect_sphere(&ray, &world.objects[sphere_index]) {
-    //             t = i.0;
-    //             // println!("{}",t);
-    //             // intersections
-    //             if t > 0.0 {
-    //                 //if the intersection is visible
-    //                 point = position(&ray, t);
-    //                 normal = normal_at(&world.objects[sphere_index], point);
-    //                 eye = -ray.direction;
-    //                 color = lighting(&mut world.objects[sphere_index].material, &light, point, eye, normal);
-    //                 canvas.write_pixel(x as usize, y as usize, color);
-    //             }
-    //         }
-    //     }
-    // }
-    // -----------------------------------------
-    // --------- Main render loop end ----------
-    // -----------------------------------------
-
-    // --------- Saving render ---------- d
+    // --------- Saving render ----------
     _save_png("test_render.png", image);
 }
